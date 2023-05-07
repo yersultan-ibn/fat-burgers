775bc679160be8d46fd99d817d7330db30353ff6
Ersultan <ersultansansyzbaj012@gmail.com>
2023-05-07 20:19:59 +0600

    Компоненты Image и Head

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,42 @@
-import Image from 'next/image';
-import Head from 'next/head';
-import Link from 'next/link';
-import styles from '../styles/Home.module.css';
+import Image from "next/image";
+import Head from "next/head";
+import Link from "next/link";
+import styles from "../styles/Home.module.css";
 
 const Home = () => {
   return (
-    <div className={styles.container}>
-      <h1 className={`${styles.title} font-effect-fire-animation`}>Главная</h1>
-      <p className={styles.text}>Что такое идеальный бургер? Лист свежего салата, мягкие булочки, сочное мясо. О других составляющих начинки можно поспорить, ведь это дело вкуса.</p>
-      <p className={styles.text}>Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество обслуживания, правильная атмосфера заведения.</p>
-      <Link href='/burgers'><a className={styles.btn}>Все бургеры</a></Link>
-    </div>
+    <>
+      <Head>
+        <title> Жирные бургеры | Главная</title>
+        <meta name="title" content="Жирные бургеры" />
+      </Head>
+      <div className={styles.container}>
+        <h1 className={`${styles.title} font-effect-fire-animation`}>
+          Главная
+        </h1>
+        <div className={styles.mainImage}>
+          <Image
+            src="/fatburger.png"
+            alt="fat burger"
+            width={400}
+            height={300}
+          />
+        </div>
+        <p className={styles.text}>
+          Что такое идеальный бургер? Лист свежего салата, мягкие булочки,
+          сочное мясо. О других составляющих начинки можно поспорить, ведь это
+          дело вкуса.
+        </p>
+        <p className={styles.text}>
+          Есть ещё пара-тройка факторов, влияющих на аппетит: цены, качество
+          обслуживания, правильная атмосфера заведения.
+        </p>
+        <Link href="/burgers">
+          <a className={styles.btn}>Все бургеры</a>
+        </Link>
+      </div>
+    </>
   );
-}
+};
 
 export default Home;
