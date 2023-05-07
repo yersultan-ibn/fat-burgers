a63e4396534944fc2b866a827887374ee3ae2ecd
Ersultan <ersultansansyzbaj012@gmail.com>
2023-05-07 20:47:04 +0600

    Используем функцию getServerSideProps для получения данных (SSR)

--- pages/reviews.js
+++ pages/reviews.js
@@ -1,37 +1,52 @@
-import Head from 'next/head';
+import Head from "next/head";
+// import { useEffect, useState } from "react";
 
-const ReviewsPage = ({ reviews }) => {
+const ReviewsPage = () => {
+  // const [reviews, setReviews] = useState();
+  // useEffect(() => {
+  //   const getData = async () => {
+  //     const response = await fetch(
+  //       "https://jsonplaceholder.typicode.com/comments"
+  //     );
+  //     const data = await response.json();
+
+  //     setRewiews(data);
+  //   };
+  //   getData();
+  // }, []);
+
   return (
     <>
       <Head>
         <title>Отзывы наших клиентов</title>
-        <meta name="title" content="Все отзывы"/>
+        <meta name="title" content="Отзывы наших клиентов" />
       </Head>
       <div>
         <h1>Отзывы клиентов</h1>
-        <div className='reviews'>
-          { !!reviews.length && reviews.map(res => {
-            return (
-              <div key={res.id} className='review'>
-                {res.id}){ ' ' }
-                {`${res.body.slice(0, 90)}...`}
-              </div>)
-          })}
+        <div className="reviews">
+          {!reviews &&
+            reviews.slice(0, 20).map((r) => {
+              return (
+                <div key={r.id} className="review">
+                  {r.id} ;{`${r.body.slice(0, 90)}...`}
+                </div>
+              );
+            })}
         </div>
       </div>
     </>
   );
-}
+};
 
 export async function getServerSideProps(context) {
-  const response =  await fetch('https://jsonplaceholder.typicode.com/comments');
+  const response = await fetch("https://jsonplaceholder.typicode.com/comments");
   const data = await response.json();
 
   return {
     props: {
-      reviews: data.slice(0, 20)
-    }
-  }
+      reviews: data.slice(0, 20),
+    },
+  };
 }
- 
+
 export default ReviewsPage;
