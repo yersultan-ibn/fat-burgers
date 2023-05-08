980e4ad49ab9a76faeb2a840ce8c04c636d36146
Ersultan <ersultansansyzbaj012@gmail.com>
2023-05-08 14:59:38 +0600

    Динамические маршруты для статических страниц (часть 2)

--- pages/burgers/[id].js
+++ pages/burgers/[id].js
@@ -1,9 +1,25 @@
+export const getStaticPaths = async () => {
+  const res = await fetch("http://localhost:5000/items");
+  const data = await res.json();
+
+const paths = data.map(burger =>{
+    return {
+        params: {id: burger.id}
+    }
+})
+
+  return {
+    paths: paths,
+    fallback: false
+  }
+};
+
 const Details = () => {
-    return (
-        <div>
-            <h1>Details</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Details</h1>
+    </div>
+  );
+};
 
 export default Details;
