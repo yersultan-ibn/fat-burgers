b586ff63a46bb1cd216f77fcf57ee8c904ebbacb
Ersultan <ersultansansyzbaj012@gmail.com>
2023-05-08 14:48:27 +0600

    Отображаю список бургеров на странице

--- pages/burgers/index.js
+++ pages/burgers/index.js
@@ -1,21 +1,47 @@
-export const getStaticProps = async () =>{
-  const res = await fetch('http://localhost:5000/items');
+import Link from "next/link";
+
+import styles from '../../styles/Burgers.module.css';
+import Image from "next/image";
+
+export const getStaticProps = async () => {
+  const res = await fetch("http://localhost:5000/items");
   const data = await res.json();
 
   return {
     props: {
-        burgers: data
-    }
-  }
+      burgers: data,
+    },
+  };
 };
 
-const Burgers = (props) => {
-
-    return (
-        <div>
-            <h1>Наши бургеры</h1>
-        </div>
-    )
-}
+const Burgers = ({burgers}) => {
+  return (
+    <div>
+      <h1>Наши бургеры</h1>
+      {burgers.map(burger =>{
+        return (
+            <Link href={`/burgers/${burger.id}`} key={burger.id}>
+                <a className={styles.burgerCard}>
+                    <div className={styles.imageContainer}>
+                        <Image 
+                        src={`${burger.image}`}
+                        alt={`${burger.name}`}
+                        width="100%"
+                        height="100%"
+                        Layout="responsive"
+                        objectFit="cover"
+                        />
+                    </div>
+                    <div>
+                        <h3>{burger.name}</h3>
+                        <p>{burger.desc}</p>
+                    </div>
+                </a>
+            </Link>
+        )
+      })}
+    </div>
+  );
+};
 
 export default Burgers;
